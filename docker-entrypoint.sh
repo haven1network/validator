5c5db861b6f268673e489f169126c2c5085c0dcb
Gaurav Jain <gaurav.jain@yield.app>
2024-04-26 18:35:08 +0530

    feat: [HVN-000] change host

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,35 +1,35 @@
 geth \
 --networkid "$NETWORKID" \
 --gcmode archive \
---password ./keystore/accountPassword \
+--password /keystore/accountPassword \
 --keystore /keystore \
 --permissioned \
 --verbosity "${VERBOSITY}" \
---datadir ./data \
+--datadir /data \
 --syncmode full \
 --port 30303 \
 --nousb \
 --identity ${TS_HOSTNAME}-validator \
---nodekey ./keystore/nodekey \
+--nodekey /keystore/nodekey \
 --nodiscover \
 --metrics \
 --pprof \
---pprof.addr 0.0.0.0\
+--pprof.addr 0.0.0.0 \
 --pprof.port 9545 \
 --istanbul.blockperiod 5 \
 --istanbul.requesttimeout 10000 \
 --http \
---http.addr 0.0.0.0\
+--http.addr 0.0.0.0 \
 --http.port 8545 \
 --http.corsdomain '*' \
 --http.vhosts '*' \
 --http.api admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,qbft \
 --ws \
---ws.addr 0.0.0.0\
+--ws.addr 0.0.0.0 \
 --ws.port 8546 \
 --ws.origins '*' \
 --ws.rpcprefix '/' \
---ws.api admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,qbft init ./data/genesis.json
+--ws.api admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,qbft init /data/genesis.json
 
 geth \
 --networkid ${NETWORKID} \
@@ -47,18 +47,18 @@ geth \
 --nodiscover \
 --metrics \
 --pprof \
---pprof.addr 127.0.0.1 \
+--pprof.addr 0.0.0.0 \
 --pprof.port 9545 \
 --istanbul.blockperiod 5 \
 --istanbul.requesttimeout 10000 \
 --http \
---http.addr 127.0.0.1 \
+--http.addr 0.0.0.0 \
 --http.port 8545 \
 --http.corsdomain '*' \
 --http.vhosts '*' \
 --http.api admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,qbft \
 --ws \
---ws.addr 127.0.0.1 \
+--ws.addr 0.0.0.0 \
 --ws.port 8546 \
 --ws.origins '*' \
 --ws.rpcprefix '/' \
